3c16ecf3ac8d8b9767f5a2e11e72155e615a5953
Wilson, Andy <wilso961@msu.edu>
2022-11-15 16:46:33 -0500

    Added some basic functions for the agent parent classes that all of the child agents will use

--- agent/agent.py
+++ agent/agent.py
@@ -3,4 +3,62 @@ class Agent:
     """
     Template for all agents to implement
     """
-    pass
+    def __init__(self, agent_name=""):
+        self.agent_name = ""
+        self.current_piece = None
+        self.current_board = None
+
+    def play_move(self):
+        """
+        Should be overloaded by child agent classes to add the need functionality for thier
+        implementation
+        @return:
+        """
+        pass
+
+    def set_current_piece(self, new_piece):
+        """
+        Sets the current player piece to the new given one.
+        @param new_piece: An int representing the new piece the player will be using (1 for x and -1
+        for 0)
+        @return:
+        """
+        self.current_piece = new_piece
+
+    def set_current_board(self, new_board):
+        """
+        Sets the current board the agent is playing to the given one.
+        @param new_board:
+        @return:
+        """
+        self.current_board = new_board
+
+    def start_game(self, board, piece):
+        """
+        When the games starts the agent will receive a reference to the board object that it is
+        playing on and the piece that it will be using. This function can be used by child agent
+        classes as is or can be overloaded.
+        @param board: A Board object containing the board that the player will be playing on
+        @param piece: An int representing the piece the agent will be playing with
+        @return: None
+        """
+        self.set_current_piece(piece)
+        self.set_current_board(board)
+
+    def end_game(self):
+        """
+        Once the game is over the agent will set it's current piece and board back to none to get
+        ready for its next game. This function can be used by child agent classes as is or can be
+        overloaded and change or add to what it does.
+        @return: None
+        """
+        self.set_current_piece(None)
+        self.set_current_board(None)
+
+    def get_available_spaces(self):
+        """
+        Should be overloaded by child agent classes to add the need functionality for thier
+        implementation
+        @return: None
+        """
+        pass
--- agent/random_agent.py
+++ agent/random_agent.py
@@ -1,4 +1,4 @@
-from agent import Agent
+from agent.agent import Agent
 
 
 class RandomAgent(Agent):
