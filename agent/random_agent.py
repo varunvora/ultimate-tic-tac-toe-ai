30d44a64ed746d6298f404dc0686decff6ffd09b
Varun Vora <varunvora@live.com>
2022-11-27 11:30:10 -0500

    add random agent

--- agent/random_agent.py
+++ agent/random_agent.py
@@ -4,4 +4,16 @@ from random import sample
 
 
 class RandomAgent(Agent):
-    pass
+    """
+    Picks a random move from the legal moves.
+    """
+
+    def __init__(self, name="Random Sample Agent"):
+        super(RandomAgent, self).__init__(agent_name=name)
+
+    def play_move(self, board: Board):
+        assert not board.is_terminal()
+        legal_moves = board.get_legal_moves()
+        move = sample(legal_moves, 1)[0]
+        ((x, y), (i, j)) = move
+        board.play(board.turn, (x, y), (i, j))
